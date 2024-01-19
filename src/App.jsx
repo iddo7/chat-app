f40a4746a3f3e18aea440ac8ebf58d1483171c2c
iddo <isaacnegreiros10@gmail.com>
2024-01-19 14:58:14 -0500

    Adding read info in room list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle as fullCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle as emptyCircle } from '@fortawesome/free-regular-svg-icons'
 
 export default function App() {
 
@@ -11,9 +14,28 @@ export default function App() {
         <section className='rooms-list-section'>
           <div className="d-flex flex-column">
 
+
             <div className="room-preview d-flex flex-row align-items-center">            
               <img className='room-img' src='https://t4.ftcdn.net/jpg/02/01/10/87/360_F_201108775_UMAoFXBAsSKNcr53Ip5CTSy52Ajuk1E4.jpg' />
-              <h2>Yappertown</h2>
+              <div className="d-flex flex-column">
+                <h2>Yappertown</h2>
+                <p>
+                  Lorem ipsum bla blu.
+                </p>
+              </div>
+                <FontAwesomeIcon icon={fullCircle} className='ms-auto icon-core' />
+            </div>
+
+
+            <div className="room-preview d-flex flex-row align-items-center">            
+              <img className='room-img' src='https://t4.ftcdn.net/jpg/02/01/10/87/360_F_201108775_UMAoFXBAsSKNcr53Ip5CTSy52Ajuk1E4.jpg' />
+              <div className="d-flex flex-column">
+                <h2>Yappertown</h2>
+                <p>
+                  Lorem ipsum bla blu.
+                </p>
+              </div>
+                <FontAwesomeIcon icon={fullCircle} className='ms-auto' />
             </div>
 
             <div className="room-preview d-flex flex-row align-items-center">            
@@ -24,6 +46,7 @@ export default function App() {
                   Lorem ipsum bla blu.
                 </p>
               </div>
+                <FontAwesomeIcon icon={emptyCircle} className='ms-auto' />
             </div>
 
           </div>
