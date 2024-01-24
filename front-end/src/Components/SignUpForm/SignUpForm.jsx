ca5b49c71d69af1d1771bb2aaa5cb834a633427a
Isaac Negreiros <isaacnegreiros10@gmail.com>
2024-01-24 11:01:45 -0500

    Form Events

--- front-end/src/Components/SignUpForm/SignUpForm.jsx
+++ front-end/src/Components/SignUpForm/SignUpForm.jsx
@@ -5,47 +5,64 @@ export default function SignUpForm() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const user = {
+            email: email,
+            password: password
+        }
+        console.log(user)
+    }
 
     return (
         <>
             <div className="wrapper bg-dark text-white p-3">
                 <h1 className="mb-3">Sign Up</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-row">
-                        <label for="email" class="form-label">Email address</label>
+                        <label htmlFor="email" className="form-label">Email address</label>
                         <input 
+                            value={email}
+                            onChange={e => {
+                                setEmail(e.target.value)
+                            }}
                             type="email" 
-                            class="form-control" 
+                            className="form-control" 
                             id="email" 
                         />
                     </div>
                     <div className="form-row">
-                        <label for="emailConfirm" class="form-label">Confirm email address</label>
+                        <label htmlFor="emailConfirm" className="form-label">Confirm email address</label>
                         <input 
                             type="email" 
-                            class="form-control" 
+                            className="form-control" 
                             id="emailConfirm" 
                         />
                     </div>
                     
                     <div className="form-row">
-                        <label for="password" class="form-label">Password</label>
+                        <label htmlFor="password" className="form-label">Password</label>
                         <input 
+                            value={password}
+                            onChange={e => {
+                                setPassword(e.target.value)
+                            }}
                             type="password" 
-                            class="form-control" 
+                            className="form-control" 
                             id="password"
                         />
                     </div>
                     <div className="form-row">
-                        <label for="passwordConfirm" class="form-label">Confirm password</label>
+                        <label htmlFor="passwordConfirm" className="form-label">Confirm password</label>
                         <input 
                             type="password" 
-                            class="form-control" 
+                            className="form-control" 
                             id="passwordConfirm" 
                         />
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         </>
