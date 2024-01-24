539bfa55b34017d261183f71f15f636132423058
iddo <isaacnegreiros10@gmail.com>
2024-01-24 16:55:35 -0500

    Adding auto-complete to form

--- front-end/src/Components/SignUpForm/SignUpForm.jsx
+++ front-end/src/Components/SignUpForm/SignUpForm.jsx
@@ -17,52 +17,56 @@ export default function SignUpForm() {
 
     return (
         <>
-            <div className="wrapper bg-dark text-white p-3">
-                <h1 className="mb-3">Sign Up</h1>
+            <div className='wrapper bg-dark text-white p-3'>
+                <h1 className='mb-3'>Sign Up</h1>
                 <form onSubmit={handleSubmit}>
-                    <div className="form-row">
-                        <label htmlFor="email" className="form-label">Email address</label>
+                    <div className='form-row'>
+                        <label htmlFor='email' className='form-label'>Email address</label>
                         <input 
                             value={email}
                             onChange={e => {
                                 setEmail(e.target.value)
                             }}
-                            type="email" 
-                            className="form-control" 
-                            id="email" 
+                            type='email' 
+                            autoComplete='email'
+                            className='form-control' 
+                            id='email' 
                         />
                     </div>
-                    <div className="form-row">
-                        <label htmlFor="emailConfirm" className="form-label">Confirm email address</label>
+                    <div className='form-row'>
+                        <label htmlFor='emailConfirm' className='form-label'>Confirm email address</label>
                         <input 
-                            type="email" 
-                            className="form-control" 
-                            id="emailConfirm" 
+                            type='email' 
+                            autoComplete='confirm-email'
+                            className='form-control' 
+                            id='emailConfirm' 
                         />
                     </div>
                     
-                    <div className="form-row">
-                        <label htmlFor="password" className="form-label">Password</label>
+                    <div className='form-row'>
+                        <label htmlFor='password' className='form-label'>Password</label>
                         <input 
                             value={password}
                             onChange={e => {
                                 setPassword(e.target.value)
                             }}
-                            type="password" 
-                            className="form-control" 
-                            id="password"
+                            type='password' 
+                            autoComplete='password'
+                            className='form-control' 
+                            id='password'
                         />
                     </div>
-                    <div className="form-row">
-                        <label htmlFor="passwordConfirm" className="form-label">Confirm password</label>
+                    <div className='form-row'>
+                        <label htmlFor='passwordConfirm' className='form-label'>Confirm password</label>
                         <input 
-                            type="password" 
-                            className="form-control" 
-                            id="passwordConfirm" 
+                            type='password' 
+                            autoComplete='confirm-password'
+                            className='form-control' 
+                            id='passwordConfirm' 
                         />
                     </div>
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
                 </form>
             </div>
         </>
